3874c7498f83b754324b7e426911b2c8de56b85e
tmichiya <239898452+tmichiya@users.noreply.github.com>
2026-08-08 15:06:39 +0900

    player move and dash

--- script/player.gd
+++ script/player.gd
@@ -8,6 +8,7 @@ enum State {
 var state: State = State.MOVE
 var move_speed: float = 200.0
 var direction: Vector2 = Vector2.ZERO
+var dash_dir: Vector2 = Vector2.ZERO
 @export var dash_duration: float = 0.2
 @export var dash_cooldown: float = 1.0
 @export var dash_speed: float = 400.0
@@ -16,22 +17,12 @@ var dash_timer: float = 0.0
 var dash_cd_timer: float = 0.0
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("dash") and state == State.MOVE and dash_timer <= 0:
+	if event.is_action_pressed("dash") and state == State.MOVE and dash_cd_timer <= 0:		
+		dash_timer = dash_duration
+		dash_cd_timer = dash_cooldown
+		dash_dir = direction
+		
 		state = State.DASH
-		print("DASH")
-		dash()
-
-func dash() -> void:
-	dash_timer = dash_duration
-	dash_cd_timer = dash_cooldown
-
-	var dash_dir = direction
-
-	while dash_timer > 0:
-		velocity = dash_dir * dash_speed
-		move_and_slide()
-
-	state = State.MOVE
 
 func timer_control(delta: float) -> void:
 	if dash_cd_timer > 0:
@@ -43,15 +34,17 @@ func timer_control(delta: float) -> void:
 func _ready() -> void:
 	state = State.MOVE
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 
 	timer_control(delta)
-	
 
 	if(state == State.DASH):
-		pass
+		velocity = dash_dir * dash_speed
+		move_and_slide()
+
+		if (dash_timer <= 0):
+			state = State.MOVE
 	elif(state == State.MOVE):
 		move_speed = 200.0
 		velocity = Input.get_vector("left", "right", "up", "down") * move_speed
