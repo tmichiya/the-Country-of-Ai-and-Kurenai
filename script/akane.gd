9c2f41cc3ac7cc75acb81441e4176951d817f9e1
tmichiya <239898452+tmichiya@users.noreply.github.com>
2026-08-10 16:05:17 +0900

    akane onagi

--- script/akane.gd
+++ script/akane.gd
@@ -3,23 +3,44 @@ extends CharacterBody2D
 enum State {
 	IDLE,
 	ATTACK,
-	CHAT
+	CHAT,
+	STUNNED
 }
+
+enum MovementState {
+	NONE,
+	DASH
+}
+
+var attacks: Array = ["karatake", "onagi"]
 
 var state: State = State.IDLE
 var state_timer: float = 1.0
+var movement_dash_timer: float = 0.0
+var movement_state: MovementState = MovementState.NONE
+var movement_state_dash_strength: float = 0
 var attack_instance: Node2D = null
 
 @export var player: CharacterBody2D
 const attack_karatake_scene: PackedScene = preload("res://scene/akane/attack_karatake.tscn")
+const attack_onagi_scene: PackedScene = preload("res://scene/akane/attack_onagi.tscn")
 
-func attack_karatake() -> void:
-	attack_instance = attack_karatake_scene.instantiate()
+func attack(attack_name: String) -> void:
+	if attack_name == "karatake":
+		attack_instance = attack_karatake_scene.instantiate()
+	elif attack_name == "onagi":
+		attack_instance = attack_onagi_scene.instantiate()
+
 	add_child(attack_instance)
 	attack_instance.global_position = global_position
 	attack_instance.attack_finished.connect(_on_attack_finished)
 
 	state = State.ATTACK
+
+	if attack_name == "onagi":
+		movement_state = MovementState.DASH
+		movement_dash_timer = 0.5
+		movement_state_dash_strength = 200
 
 func _on_attack_finished() -> void:
 	attack_instance = null
@@ -27,7 +48,6 @@ func _on_attack_finished() -> void:
 	state_timer = randf_range(1.0, 3.0)
 
 func is_telegraphing() -> bool:
-
 	print("Checking if attack_instance is telegraphing: %s" % (attack_instance != null and attack_instance.has_method("is_playing_telegraph_animation")))
 	if attack_instance and attack_instance.has_method("is_playing_telegraph_animation"):
 		return attack_instance.is_playing_telegraph_animation()
@@ -45,7 +65,17 @@ func _physics_process(delta: float) -> void:
 		if state_timer > 0.0:
 			state_timer -= delta
 		else:
-			attack_karatake()
+			var random_attack = attacks[randi() % attacks.size()]
+			attack(random_attack)
 			state_timer = randf_range(1.0, 3.0)
+
+	if movement_state == MovementState.DASH:
+		movement_dash_timer -= delta
+		if movement_dash_timer <= 0:
+			movement_state = MovementState.NONE
+			movement_state_dash_strength = 0
+			velocity = Vector2.ZERO
+		else:
+			velocity = Vector2(cos(rotation), sin(rotation)) * movement_state_dash_strength * movement_dash_timer
 
 	move_and_slide()
---
+++ script/attack_onagi.gd
@@ -0,0 +1,41 @@
+extends Node2D
+signal attack_finished
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var hit_box: Area2D = $HitBox
+@export var damage: int = 20
+
+var is_telegraphing: bool = false
+
+func enable_hitbox() -> void:
+	hit_box.get_node("CollisionPolygon2D").set_deferred("disabled", false)
+
+func disable_hitbox() -> void:
+	hit_box.get_node("CollisionPolygon2D").set_deferred("disabled", true)
+
+func is_playing_telegraph_animation() -> bool:
+	return is_telegraphing
+
+func switch_is_telegraphing_to(value: bool) -> void:
+	is_telegraphing = value
+
+func _on_hitbox_area_entered(area: Area2D) -> void:
+	if not area.is_in_group("player"):
+		return
+
+	var player = area.get_parent() as CharacterBody2D
+	if player.has_method("take_damage"):
+		player.take_damage(damage)
+
+func _on_animation_finished(anim_name: String) -> void:
+	if anim_name == "attack_onagi":
+		print("Attack Onagi animation finished")
+		attack_finished.emit()
+		queue_free()
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	animation_player.play("attack_onagi")
+	animation_player.animation_finished.connect(_on_animation_finished)
+
+	hit_box.area_entered.connect(_on_hitbox_area_entered)
